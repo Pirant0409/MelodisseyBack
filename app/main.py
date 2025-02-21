31178c36d23b5f98f200b89df63f147f307f3e17
pirant0409 <ant.mau.pir@gmail.com>
2025-02-21 21:50:33 +0100

    updating corsmiddleware

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ database.init_db()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200","http://127.0.0.1:4200","https://melodissey.e-kot.be/"],
+    allow_origins=["http://localhost:4200","http://127.0.0.1:4200","https://melodissey.e-kot.be","https://melodissey.e-kot.be/"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization","Accept"]
